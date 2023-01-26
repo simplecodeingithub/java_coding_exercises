806c4766c9ffa01ff8fe0424312092d7c409b2b4
chaitra <chaitrachubz456@gmail.com>
2023-01-26 11:42:30 +0000

    Improved the code in Exercise 3.

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -2,21 +2,19 @@ package com.techreturners.exercise003;
 
 public class Exercise003 {
 
+    String[] flavours={ "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
     int getIceCreamCode(String iceCreamFlavour) {
-        if (iceCreamFlavour.equals("Raspberry Ripple")) {
-            return 1;
-        } else if (iceCreamFlavour.equals("Mint Chocolate Chip")) {
-            return 3;
-        } else if (iceCreamFlavour.equals("Mango Sorbet")) {
-            return 5;
-        } else {
-            throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        for(int i =0;i<flavours.length;i++){
+            if(flavours[i].equalsIgnoreCase(iceCreamFlavour)){
+                return i;
+            }
         }
+        return -1;
     }
-    String[] iceCreamFlavours() {
+   String[] iceCreamFlavours() {
         String[] iceCreamFlavours={ "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
-        return iceCreamFlavours;
-        //throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return flavours;
+
     }
 
 }
